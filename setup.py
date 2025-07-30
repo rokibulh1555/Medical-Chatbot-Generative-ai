8b56cdafb32268d1bb2fbe32cfa4a188260b31d5
rokib <rokibulh1555@gmail.com>
2025-07-30 17:39:44 +0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Rokibul Hasan',
+    author_email='rokibulh1555@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
